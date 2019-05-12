3104136518dcdd350bcb6a309bfd37f2c43ad2c2
Natasha Henry <natashaphenry@yahoo.com>
2019-05-12 19:37:59 -0400

    added html

---
+++ logic.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBMikFVV8VXHSBYQM5-DcpKKqHNwWs6_Ew",
+    authDomain: "tots-project-578bc.firebaseapp.com",
+    databaseURL: "https://tots-project-578bc.firebaseio.com",
+    projectId: "tots-project-578bc",
+    storageBucket: "tots-project-578bc.appspot.com",
+    messagingSenderId: "581210930702",
+    appId: "1:581210930702:web:d31c677b175c1d86"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
